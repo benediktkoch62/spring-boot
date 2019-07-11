e892545a7f063cd163c707a3a1f3003b31366fcb
Benedikt Koch <51399886+benediktkoch62@users.noreply.github.com>
2019-07-11 11:50:18 +0200

    Update HelloController.java

--- initial/ src/ main/ java/ hallo/HelloController.java
+++ initial/ src/ main/ java/ hallo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hallo Irina...ich liebe dich";
+        return "Hi Manuel, Chinese food is brilliant :-)";
     }
     
 }
